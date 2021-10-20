04967c50b52f4688adab188eed6fafbe4c6b339e
annu12340 <annujolly17@gmail.com>
2021-10-20 19:39:57 +0530

    added google analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { useEffect } from "react";
 import IbetoJnr from "./components/ibetoJnr/IbetoJnr";
+import ReactGA from "react-ga";
+
+ReactGA.initialize("UA-205580679-1");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App({ hideLoader }) {
    useEffect(() => {
