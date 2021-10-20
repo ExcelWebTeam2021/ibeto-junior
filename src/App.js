7295cd29c8e85464e35a3ce93e324212a1000459
annu12340 <annujolly17@gmail.com>
2021-10-20 19:42:05 +0530

    google a

--- src/App.js
+++ src/App.js
@@ -1,9 +1,5 @@
 import React, { useEffect } from "react";
 import IbetoJnr from "./components/ibetoJnr/IbetoJnr";
-import ReactGA from "react-ga";
-
-ReactGA.initialize("UA-205580679-1");
-ReactGA.pageview(window.location.pathname + window.location.search);
 
 function App({ hideLoader }) {
    useEffect(() => {
